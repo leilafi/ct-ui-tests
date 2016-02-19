3b003e6792660acf66377570913122969d0421f0
leilafi <leila.firouz@red-badger.com>
2016-02-19 11:05:54 +0000

    test commit from pycharm

--- ui-tests/page_objects/argus_homepage.py
+++ ui-tests/page_objects/argus_homepage.py
@@ -5,6 +5,7 @@ import selenium
 class ArgusHomepage(base_page.BasePage):
 
     def __init__(self):
+        # initialising
         base_page.BasePage.__init__(self)
         self.page_name = "argushomepage"
         self.city_key = "popular-city"
