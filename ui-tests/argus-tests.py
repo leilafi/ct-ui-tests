34f0c6a3db99571e688504108d6730b82b532edf
leilafi <leila.firouz@red-badger.com>
2016-02-24 15:45:10 +0000

    Added a test for ContactUs page and updated destination tests.

--- ui-tests/argus-tests.py
+++ ui-tests/argus-tests.py
@@ -1,54 +1,56 @@
-from helpers import verify_things, get_elements
-from page_objects import argus_homepage, argus_location_page, argus_destination_page, argus_car_hire_page, argus_language_menu
+from page_objects import argus_homepage, argus_location_page, argus_destination_page, argus_car_hire_page, argus_language_menu, argus_contact_us_page
 import base_test
 import unittest
 
 #tests = []
 class ArgusTests(base_test.BaseTest):
 
-    def test_argus_verify_homepage_title(self):
-        this_homepage = argus_homepage.ArgusHomepage()
-        this_homepage.land_page(this_homepage.page_name)
-        self.verify_title(this_homepage.get_title(),
-                          this_homepage.page_name,
-                          this_homepage.title_key)
-
-    def test_argus_homepage_verify_locations(self):
-        this_page = argus_homepage.ArgusHomepage() # Loads homepage and then redirects to a location page
-        self.verify_all_location_links(this_page)
-
-    def test_argus_destination_exist(self):
-        this_location = argus_location_page.Location_Page()
-        this_location.land_page(this_location.page_name)
-        destination = this_location.find_element(this_location.page_name, this_location.destination_key)
-        self.assert_not_exist(destination,this_location.destination_key, this_location.page_name)
-
-    def test_argus_car_hire_verify_locations(self):
-        this_page = argus_car_hire_page.CarHirePage()
-        self.verify_all_location_links(this_page)
-
-    def test_argus_change_language_popular(self):
-        language_menu = argus_language_menu.LanguageMenu()
-        language_menu.land_page(language_menu.page_name)
-        language_menu.open_menu()
-        language_menu.select_language(language_menu.popular_language_item)
-        language_menu.verify_translation(language_menu.popular_expected_translation)
-
-    def test_argus_change_language(self):
-        language_menu = argus_language_menu.LanguageMenu()
-        language_menu.land_page(language_menu.page_name)
-        language_menu.open_menu()
-        language_menu.select_language(language_menu.language_item)
-        language_menu.verify_translation(language_menu.expected_translation)
-
-    # def test_argus_destination_load(self):
-    #     this_destination = argus_destination_page.DestinationPage()
+    # def test_argus_verify_homepage_title(self):
+    #     this_homepage = argus_homepage.ArgusHomepage()
+    #     this_homepage.land_page(this_homepage.page_name)
+    #     self.verify_title(this_homepage.get_title(),
+    #                       this_homepage.page_name,
+    #                       this_homepage.title_key)
+    #
+    # def test_argus_homepage_verify_locations(self):
+    #     this_page = argus_homepage.ArgusHomepage() # Loads homepage and then redirects to a location page
+    #     self.verify_all_location_links(this_page)
+    #
+    # def test_argus_destination_exist(self):
     #     this_location = argus_location_page.Location_Page()
     #     this_location.land_page(this_location.page_name)
-    #     this_location.destination_click()
-    #     self.verify_title(this_location.get_title(),
-    #                       this_destination.page_name,
-    #                       this_location.title_key, True)
+    #     self.assert_not_exist(this_location.find_destination_link(),this_location.page_name,this_location.destination_key)
+    #
+    # def test_argus_car_hire_verify_locations(self):
+    #     this_page = argus_car_hire_page.CarHirePage()
+    #     self.verify_all_location_links(this_page)
+    #
+    # def test_argus_change_language_popular(self):
+    #     language_menu = argus_language_menu.LanguageMenu()
+    #     language_menu.land_page(language_menu.page_name)
+    #     language_menu.open_menu()
+    #     language_menu.select_language(language_menu.popular_language_item)
+    #     language_menu.verify_translation(language_menu.popular_expected_translation)
+    #
+    # def test_argus_change_language(self):
+    #     language_menu = argus_language_menu.LanguageMenu()
+    #     language_menu.land_page(language_menu.page_name)
+    #     language_menu.open_menu()
+    #     language_menu.select_language(language_menu.language_item)
+    #     language_menu.verify_translation(language_menu.expected_translation)
+
+    def test_argus_destination_load(self):
+        this_destination = argus_destination_page.DestinationPage()
+        this_destination.land_page_from_parent()
+        self.verify_title(this_destination.get_header(),
+                          this_destination.page_name,
+                          this_destination.title_key, True)
+
+    def test_argus_contact_us_title(self):
+        this_contact = argus_contact_us_page.ContactUsPage()
+        this_contact.land_page_from_parent()
+        self.verify_title(this_contact.get_header(), this_contact.page_name,this_contact.title_key)
+
 
 if __name__ == '__main__':
     unittest.main()
--- ui-tests/page_objects/argus_destination_page.py
+++ ui-tests/page_objects/argus_destination_page.py
@@ -6,3 +6,14 @@ class DestinationPage(base_page.BasePage):
     def __init__(self):
         base_page.BasePage.__init__(self)
         self.page_name = "argusdestination"
+        self.page_header = "page-header"
+        self.parent_page = "arguslocation"
+        self.link_name = "airport"
+
+    def land_page_from_parent(self):
+        self.land_page(self.parent_page)
+        self.element_click(self.link_name, self.find_element(self.parent_page, self.link_name))
+
+    def get_header(self):
+        header = self.find_element(self.page_name, self.page_header)
+        return header.text
